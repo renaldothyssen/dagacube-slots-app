5c7754383d668077ce5a5c6cdba087f652ab8029
thyssenr <renaldo.thyssen@mmiholdings.co.za>
2022-03-17 21:44:30 +0200

    First Commit

---
+++ src/main/java/com/dagacube/DagacudeSlotsAppApplication.java
@@ -0,0 +1,13 @@
+package com.dagacube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagacudeSlotsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagacudeSlotsAppApplication.class, args);
+	}
+
+}
